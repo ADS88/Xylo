81d514a98c32cefb62a15c04805c5ced9b9a8047
Andrew Sturman <adsturmanator88@gmail.com>
2020-12-24 18:16:53 +1300

    added main and high score screens

--- Xylophone/Controller/GameOverViewController.swift
+++ Xylophone/Controller/GameOverViewController.swift
@@ -12,6 +12,14 @@ class GameOverViewController: UIViewController {
     @IBOutlet weak var scoreText: UILabel!
     var score: Int = 0
     
+    @IBAction func playAgainPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "playAgain", sender: self)
+    }
+    
+    @IBAction func goToMenuPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToMenu", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scoreText.text = "Score: \(score)"
---
+++ Xylophone/Controller/MenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MenuViewController.swift
+//  Xylophone
+//
+//  Created by Andrew on 24/12/20.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBAction func playButtonClicked(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToGame", sender: self)
+    }
+    
+    @IBAction func highScoreButtonClicked(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToHighScore", sender: self)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
