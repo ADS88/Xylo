d6a9306d7b95d8ac0214a873ca0f23d8b372191f
Andrew Sturman <adsturmanator88@gmail.com>
2021-01-04 21:02:16 +1300

    Added tap on gamemode to play and song choices only showing when scrolling over song mode

--- Xylophone/Controller/GameOptionsViewController.swift
+++ Xylophone/Controller/GameOptionsViewController.swift
@@ -8,30 +8,57 @@
 import UIKit
 import iCarousel
 
-class GameOptionsViewController: UIViewController, iCarouselDataSource {
-    
-    let gameModes = [GameModeOption(title: "Memory", description: "Notes will appear to be tapped on the screen. Tap the correct notes back to gain points. The amount of notes to remember increases over time ", color: UIColor(named: "xyloPurple") ?? .purple, imageName: "music.quarternote.3"),
-                     GameModeOption(title: "Pitch Detection", description: "Notes will be played without visual indication. Tap the correct notes back to gain points. The amount of notes to remember increases over time", color: UIColor(named: "xyloBlue") ?? .blue, imageName: "music.note"),
-                     GameModeOption(title: "Song Mode", description: "Notes of a chosen song will be played visual indication. Tap the correct notes back to gain points. The amount of notes to remember increases over time", color: UIColor(named: "xyloOrange") ?? .orange, imageName: "music.note.list")
-    ]
+class GameOptionsViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
     
     @IBOutlet weak var carousel: iCarousel!
+    @IBOutlet weak var songPickerView: UIPickerView!
+    
+    let songNames = ["Mary had a little lamb", "La Bamba", "Mo Bamba", "Bobby Shmurda"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(carousel)
         carousel.type = .rotary
         carousel.dataSource = self
+        carousel.delegate = self
+        songPickerView.delegate = self
+        songPickerView.dataSource = self
+        updatePickerVisibility()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func startButtonClicked(_ sender: UIButton) {
+    
+    @objc
+    func startGame(){
         self.performSegue(withIdentifier: "goToGame", sender: self)
+    }
+    
+    
+    let gameModes = [GameModeOption(title: "Memory", description: "Notes will appear to be tapped on the screen. Tap the correct notes back to gain points. The amount of notes to remember increases over time ", color: UIColor(named: "xyloPurple") ?? .purple, imageName: "music.quarternote.3"),
+                     GameModeOption(title: "Pitch Detection", description: "Notes will be played without visual indication. Tap the correct notes back to gain points. The amount of notes to remember increases over time", color: UIColor(named: "xyloBlue") ?? .blue, imageName: "music.note"),
+                     GameModeOption(title: "Song Mode", description: "Notes of a chosen song will be played visual indication. Tap the correct notes back to gain points. The amount of notes to remember increases over time", color: UIColor(named: "xyloOrange") ?? .orange, imageName: "music.note.list")
+    ]
+    
+    
+    func carouselDidScroll(_ carousel: iCarousel) {
+        updatePickerVisibility()
     }
     
     func numberOfItems(in carousel: iCarousel) -> Int {
         return 3
     }
+    
+    func updatePickerVisibility(){
+        if carousel.currentItemIndex == GameMode.SONG.rawValue {
+            songPickerView.isHidden = false
+        } else {
+            songPickerView.isHidden = true
+        }
+    }
+    
+    func carouselCurrentItemIndexDidChange(_ carousel: iCarousel) {
+            updatePickerVisibility()
+      }
     
     
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
@@ -40,6 +67,8 @@ class GameOptionsViewController: UIViewController, iCarouselDataSource {
         view.layer.cornerRadius = 20
         view.frame.size = CGSize(width: self.view.frame.size.width/1.2, height: self.view.frame.size.height / 1.4)
         view.backgroundColor = gameModes[index].color
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.startGame))
+        view.addGestureRecognizer(tapGesture)
 
         let image = UIImage(systemName: gameModes[index].imageName)
         view.optionImage.image = image
@@ -63,3 +92,21 @@ class GameOptionsViewController: UIViewController, iCarouselDataSource {
         }
     }
 }
+
+
+extension GameOptionsViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+         return songNames[row]
+    }
+}
+
+extension GameOptionsViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return songNames.count
+    }
+}
+
