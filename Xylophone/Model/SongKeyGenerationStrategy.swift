582ad85a7ebc0afcb51a428611f6c4ad9c825e02
Andrew Sturman <adsturmanator88@gmail.com>
2021-01-23 12:57:22 +1300

    Changed song generation strategy to repeat more of the song each time, rather than just different parts

--- Xylophone/Model/SongKeyGenerationStrategy.swift
+++ Xylophone/Model/SongKeyGenerationStrategy.swift
@@ -9,29 +9,23 @@ import Foundation
 
 struct SongKeyGenerationStrategy : KeyGenerationStrategy {
     
-    var currentIndex = 0
     var notes = [String]()
     var sounds = [String]()
-    var leastKeysPlayed = 1
-    var mostKeysPlayed = 2
+    var keysToPlay = 1
     
     mutating func generateKeys() -> [String] {
+        var i = 0
         var items = [String]()
-        var numKeys = Int.random(in: leastKeysPlayed...mostKeysPlayed)
-        while currentIndex < notes.count && numKeys > 0 {
-            items.append(notes[currentIndex])
-            currentIndex += 1
-            numKeys -= 1
+        keysToPlay = Int.random(in: keysToPlay + 1...keysToPlay + 3)
+        while i < min(notes.count, keysToPlay){
+            items.append(notes[i])
+            i += 1
         }
-        leastKeysPlayed += 1
-        mostKeysPlayed += 1
         return items
     }
     
     mutating func reset(){
-        currentIndex = 0
-        leastKeysPlayed = 1
-        mostKeysPlayed = 2
+        keysToPlay = 1
     }
     
     init(notes: [String], sounds: [String]) {
